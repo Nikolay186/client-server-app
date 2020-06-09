710a44acd2d8a4bd90e4ae47a5860969c004a2cf
Nikolay <63589623+Nikolay186@users.noreply.github.com>
2020-06-09 19:26:59 +0500

    First version

---
+++ Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding(866);
+            try
+            {
+                StartConnection("localhost", 8880);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void StartConnection(string hostName, int port)
+        {
+            byte[] recieve = new byte[1024];
+
+            IPHostEntry host = Dns.GetHostEntry(hostName);
+            IPAddress ip = host.AddressList[0];
+            IPEndPoint ipEnd = new IPEndPoint(ip, port);
+
+            Socket transferSocket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            transferSocket.Connect(ipEnd);
+
+            Console.WriteLine("Enter your message: ");
+            string msg = Console.ReadLine();
+
+            Console.WriteLine($"Connecting to port {transferSocket.RemoteEndPoint}");
+
+            byte[] sendMsg = Encoding.UTF8.GetBytes(msg);
+            int bytesSent = transferSocket.Send(sendMsg);
+            int bytesRecieved = transferSocket.Receive(recieve);
+
+            Console.WriteLine($"Server's reply: {Encoding.UTF8.GetString(recieve, 0, bytesRecieved)}");
+
+            if (msg.IndexOf("shutdown") == -1)
+            {
+                StartConnection(hostName, port);
+            }
+            transferSocket.Shutdown(SocketShutdown.Both);
+            transferSocket.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static int port = 8880;
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding(866);
+            Console.WriteLine("The server is running");
+
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ip = host.AddressList[0];
+            IPEndPoint ipEnd = new IPEndPoint(ip, port);
+
+            Socket socket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Bind(ipEnd);
+                socket.Listen(10);
+
+                while(true)
+                {
+                    Console.WriteLine($"Listening port {ipEnd}");
+                    Socket s = socket.Accept();
+
+                    string response = null;
+
+                    byte[] data = new byte[1024];
+                    int dataLength = s.Receive(data);
+                    response += Encoding.UTF8.GetString(data, 0, dataLength);
+                    Console.WriteLine($"Retrieving response... \r\n {response}");
+
+                    string reply = "Query size: " + response.Length.ToString();
+
+                    byte[] replyMsg = Encoding.UTF8.GetBytes(reply);
+                    s.Send(replyMsg);
+
+                    if (response.IndexOf("shutdown") > -1)
+                    {
+                        Console.WriteLine("Connection closed.");
+                        break;
+                    }
+                    s.Shutdown(SocketShutdown.Both);
+                    s.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
